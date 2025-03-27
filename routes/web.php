406e1218056126fbdb126624f2233ab2c6a0362a
rodriiii94 <rodrisantaga94@gmail.com>
2025-03-27 22:44:10 +0100

    api funcionando

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 Route::get('/', function () {
-    return view('primera');
+    //return view('primera');
+    $response = Http::get('http://localhost:3000/api/data');
+    $data = $response->json();
+    $cont = 1;
+    foreach ($data as $product) {
+        echo $cont . " - " . $product['name'] . " - " . $product['id'] . '<br>';
+        $cont++;
+    }
+    //dd($data);
 });
